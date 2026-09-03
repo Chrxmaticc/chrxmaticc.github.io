3c434e3659d24e372d29f20c49b0a867076f2004
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-09-03 14:10:42 -0400

    Fix import path for MeshGradient component

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MeshGradient } from "@/components/(global)/GradientMesh";
+import { MeshGradient } from "../components/(global)/GradientMesh";
 import { motion } from "framer-motion";
 import {
     ChevronDown,
